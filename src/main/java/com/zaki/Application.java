2e859115e30aed1b6bae5b523c1fcf4b06191c43
Mohammed Ahmed Zakiuddin <mohammedahmed.zakiuddin@mavs.uta.edu>
2025-06-23 01:13:10 -0500

    Final Updates - Phase1

--- src/main/java/com/zaki/Application.java
+++ src/main/java/com/zaki/Application.java
@@ -2,12 +2,22 @@ package com.zaki;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-
+	/*
+		 To expose this method as en endpoint we needed to add the @GetMapping Annotations.
+		 This method here is a Rest EndPoint.
+	 */
+	@GetMapping
+	public String helloWorld(){
+		return "HelloWorld Spring Boot";
+	}
 }
---
+++ src/main/java/com/zaki/SoftwareEngineerService.java
@@ -0,0 +1,21 @@
+package com.zaki;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class SoftwareEngineerService {
+
+    private final SoftwareEngineerRepository softwareEngineerRepository;
+
+    public SoftwareEngineerService(SoftwareEngineerRepository softwareEngineerRepository) {
+        this.softwareEngineerRepository = softwareEngineerRepository;
+    }
+    public List<SoftwareEngineer> getAllSoftwareEngineers(){
+        return softwareEngineerRepository.findAll();
+    }
+    public void insertSoftwareEngineer(SoftwareEngineer softwareEngineer){
+        softwareEngineerRepository.save(softwareEngineer);
+    }
+}
